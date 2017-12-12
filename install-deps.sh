1be2ae3109842b923a7e992f0d16714e24b9fc9b
Noah Watkins <noahwatkins@gmail.com>
2017-12-12 13:12:31 -0800

    asdf
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- install-deps.sh
+++ install-deps.sh
@@ -38,10 +38,26 @@ function debs() {
 }
 
 function rpms() {
+  yumdnf="yum"
+  if command -v dnf > /dev/null; then
+    yumdnf="dnf"
+  fi
+
+  ${yumdnf} install -y redhat-lsb-core
+  case $(lsb_release -si) in
+    CentOS)
+      MAJOR_VERSION=$(lsb_release -rs | cut -f1 -d.)
+      $SUDO yum-config-manager --add-repo https://dl.fedoraproject.org/pub/epel/$MAJOR_VERSION/x86_64/
+      $SUDO yum install --nogpgcheck -y epel-release
+      $SUDO rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-$MAJOR_VERSION
+      $SUDO rm -f /etc/yum.repos.d/dl.fedoraproject.org*
+      ;;
+  esac
+
   if [ -n "${USE_CLANG}" ]; then
     extra=clang
   fi
-  dnf install -y \
+  ${yumdnf} install -y \
     cmake \
     gcc-c++ \
     make \
