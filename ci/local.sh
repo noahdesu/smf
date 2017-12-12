25978376265182fa9768b59118fa647e5fa4c631
Noah Watkins <noahwatkins@gmail.com>
2017-12-12 15:13:00 -0800

    fix path
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- ci/local.sh
+++ ci/local.sh
@@ -18,8 +18,8 @@ debian:buster
 
 for img in ${IMAGES}; do
   if [[ ${img} != -* ]]; then
-    DOCKER_IMAGE=${img} USE_NINJA=  ${THIS_DIR}/ci/script.sh
-    DOCKER_IMAGE=${img} USE_NINJA=1 ${THIS_DIR}/ci/script.sh
+    DOCKER_IMAGE=${img} USE_NINJA=  ${THIS_DIR}/script.sh
+    DOCKER_IMAGE=${img} USE_NINJA=1 ${THIS_DIR}/script.sh
     # clang issues in seastar...
     #DOCKER_IMAGE=${img} USE_CLANG=1 ${THIS_DIR}/ci/script.sh
   fi
